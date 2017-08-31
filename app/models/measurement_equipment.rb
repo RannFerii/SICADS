03ba49210f83b12c847100db7cb89efa4f806ab5
Tania T <tania.torresa@hotmail.com>
2017-08-31 13:10:10 -0500

    .

--- app/models/measurement_equipment.rb
+++ app/models/measurement_equipment.rb
@@ -1,3 +1,4 @@
 class MeasurementEquipment < ApplicationRecord
-  has_many :calibrations
+  has_many :calibrations, dependent: :destroy
+  enum equipo_medicion_type: ['Termómetros', 'Multímetro','Densimetro','Volmetro' ,'Medidor de Factor de Potencia','Medidor de Resistencia de Aislamiento','Resistencia Óhmnica de devanado','Medidor de Rigidez Dieléctrica','Medidor Resistencia de Devanado','Corriente de Exitación','Tiempo de Sincronismo']
 end
